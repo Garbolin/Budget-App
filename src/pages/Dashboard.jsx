dc263c74bf9ef6d690244048125d7d1772e04bf2
Gabriela <gabrielabuilcl@gmail.com>
2024-07-08 20:15:03 +0200

    Created the Loader function for userName in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,23 @@
-//loader
+//rrd imports
+import { useLoaderData } from "react-router-dom";
 
-export function dashboardLoader() {}
+// helper functions
+import { fetchData } from "../helpers";
+
+//loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { userName } = useLoaderData();
+  return (
+    <div>
+      <h1>{userName}</h1>
+      Dashboard
+    </div>
+  );
 };
 
 export default Dashboard;
