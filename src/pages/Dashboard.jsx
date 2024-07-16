a2022bd3046bcc505d97d5a360c30a4acaee4542
Gabriela <gabrielabuilcl@gmail.com>
2024-07-16 13:59:11 +0200

    lesson 10 finished, new expense form. Lots of bugs to be fixed: button back doesn't work, budget category wrong, error pops up when submitting a new expense

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,12 @@
 import { useLoaderData } from "react-router-dom";
 
 // helper functions
-import { createBudget, fetchData, waait } from "../helpers";
+import { createBudget, createExpense, fetchData, waait } from "../helpers";
 
 //components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
 
 //library
 import { toast } from "react-toastify";
@@ -48,6 +49,19 @@ export async function dashboardAction({ request }) {
       throw new Error("There was a problem creating your budget.");
     }
   }
+  if (_action === "createExpense") {
+    try {
+      //create expense
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget,
+      });
+      return toast.success(`Expense ${values.newExpense} created!`);
+    } catch (e) {
+      throw new Error("There was a problem creating your expense.");
+    }
+  }
 }
 
 const Dashboard = () => {
@@ -60,11 +74,19 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            <div className="grid-gl">
-              <div className="flex-gl">
-                <AddBudgetForm />
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-gl">
+                <div className="flex-gl">
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secreet to financial freedom.</p>
+                <p>Create a budget to get started!</p>
+              </div>
+            )}
           </div>
         </div>
       ) : (
